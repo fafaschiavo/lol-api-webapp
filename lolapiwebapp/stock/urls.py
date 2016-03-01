947b0a02137360b6be702b85d8a77fcbd25a3fb4
fafaschiavo <fayschiavo@gmail.com>
2016-03-01 01:47:45 -0300

    id search id view added

--- lolapiwebapp/stock/urls.py
+++ lolapiwebapp/stock/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^getid/$', views.getSummonerId, name='getSummonerId'),
+    url(r'^getid/requestid/$', views.requestId, name='requestId'),
 ]
--- lolapiwebapp/stock/views.py
+++ lolapiwebapp/stock/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json, requests
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def getSummonerId(request):
+    context = {}
+    return render(request, 'getid.html', context)
+
+def requestId(request):
+    template_form = request.POST['requestId']
+    summoner_name = str(template_form)
+
+    api_key='93ecac97-49b3-4639-a4b8-51421bd89855'
+    region='br'
+    
+    url = 'https://na.api.pvp.net/api/lol/'+ region +'/v1.4/summoner/by-name/'+ summoner_name +'?api_key=' + api_key
+    
+    resp = requests.get(url=url)
+    data = json.loads(resp.text)
+    context = {}
+
+    try:
+    	context['success'] = 1
+    	context['summonerName'] = summoner_name
+    	context['summonerLevel'] = data[summoner_name]['summonerLevel']
+    	context['id'] = data[summoner_name]['id']
+    	context['profileIcon'] = data[summoner_name]['profileIconId']
+    	print context
+    	print url
+    	return render(request, 'requestid.html', context)
+    except KeyError, e:
+    	context['success'] = 0
+    	return render(request, 'requestid.html', context)
+
+
+#https://na.api.pvp.net/api/lol/br/v1.4/summoner/by-name/HelloKitter?api_key=93ecac97-49b3-4639-a4b8-51421bd89855
